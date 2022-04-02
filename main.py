a3e41173e649ec4c60d8b631e9a34e952cf95202
Jake Armstrong <Lainkai@users.noreply.github.com>
2022-04-02 16:37:39 -0400

    No async_serial wheel for RPi

--- main.py
+++ main.py
@@ -11,59 +11,33 @@ Websockets: https://websockets.readthedocs.io/en/stable/reference/client.html
 
 """
 
-import asyncio
-from functools import partial
-import serial_asyncio
+from pickle import UnpicklingError
 import pickle
-import json
+import serial
 import websockets
+import asyncio
+import concurrent.futures
 
-class ReaderProtocol(asyncio.Protocol):
+ser = serial.Serial('/dev/ttyUSB0', baudrate=115200,timeout=0.5)
+messages_received = 0
 
-    def __init__(self, queue):
-        super.__init__()
-        self.transport = None
-        self.queue = queue
-
-    """Stores serial data and prepares it to recieve"""
-    def connection_made(self, transport):
-        self.transport = transport
-        self.buf = bytes()
-        self.msgs_received = 0
-
-    async def data_received(self, data: bytes):
-        """Stores received data until we receive a `.`"""
-        self.buf += data
-        if b'.' in self.buf:
-            pickles = self.buf.split(b'.')
-            for message in pickles[:-1]:
-                # Decode Message
-                depickle = pickle.loads(message)
-                #Send message to queue
-                asyncio.ensure_future(self.queue.put(depickle))
-
-async def queue_messenger(websocket, queue):
+async def process_packets(socket):
     while True:
-        next_message = await queue.get()
-        data = json.dumps(next_message)
-        websocket.send(data)
+        buf = bytearray(0)
+        while b'.' not in buf:
+            buf.append(ser.read())
+
+        try:
+            data = pickle.loads(buf)
+            await socket.send({"status":"successful","id":messages_received,"data":data})
+        except UnpicklingError:
+            {"status":"packet lost", "id": messages_received}
+        messages_received += 1
 
 async def main():
-    #Get event loop
-    loop = asyncio.get_running_loop()
-
-    #Set up queue
-    queue = asyncio.Queue()
-    reader = partial(ReaderProtocol, queue)
-
-    #Create serial connection
-    coro = await serial_asyncio.create_serial_connection(loop, reader, '/dev/ttyS0', baudrate=115200)
-
-    trasnport, protocol = loop.run_until_complete(coro)
-    async for websocket in websockets.connnect("ws://127.0.0.1:8000/ws/telemetry/PiLoad/send",open_timeout = None):
+    async for socket in websockets.connect("ws://localhost:8000/ws/telemetry/PiLoad/send"):
         try:
-            queue_messenger(websocket, queue)
+            await process_packets( socket)
         except websockets.ConnectionClosed:
+            #automatically reconnects
             continue
-
-asyncio.run(main())
